6637761134e77bc23f67db1774226f1e47540d16
rivwox <augustoriverag@protonmail.com>
2020-12-29 14:39:19 -0600

    a simple password generator

---
+++ pass.py
@@ -0,0 +1,43 @@
+import string
+import random
+
+letters = string.ascii_letters
+numbs = string.digits
+special_c = string.punctuation
+
+def gen_pass(length):
+    '''
+    
+    '''
+    if type(length) != int:
+        pswd="Length must be a Number"
+        
+    elif length <= 8:
+        pswd="Must be longer than 8"
+    else:
+        strg = f'{letters}{numbs}{special_c}'
+
+        strg = list(strg)       #convert string into list then shuffle
+        random.shuffle(strg)
+
+        pswd = random.choices(strg, k=length)
+        pswd = ''.join(pswd)
+    return pswd
+
+def pass_length():
+    ''' 
+    Get the length of the password we want to create.
+    '''
+    length = input("Password Length: ")  
+    try:
+        length = int(length)
+        return length
+    except ValueError: 
+        return length
+    return length
+
+pass_len = pass_length()
+password = gen_pass(pass_len)
+print(password)
+
+
